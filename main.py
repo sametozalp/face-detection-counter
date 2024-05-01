11ac8cfa2230050f2a70c86987e328570c0519a8
sametozalp <sametozalp0056@gmail.com>
2024-05-01 18:57:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import torch
 from tracker import *
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-
 cap=cv2.VideoCapture(0)
-
-cv2.namedWindow('FRAME')
-
 tracker = Tracker()
 
 while True:
@@ -26,12 +22,12 @@ while True:
              list.append([x1,y1,x2,y2])
     kisi_sayisi = tracker.update(list)
     for box_id in kisi_sayisi:
-        x,y,w,h,id=box_id        
-        cv2.rectangle(frame,(x,y),(w,h),(255,0,255),2)
+        x,y,w,h,id=box_id
+        cv2.rectangle(frame, (x,y), (w,h), (255,0,255), 2)
     
     print("Kişi Sayisi: ", len(kisi_sayisi))  
-    cv2.putText(frame, "Kisi Sayisi: " + str(len(kisi_sayisi)), (20,60), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3) 
-    cv2.imshow('FRAME',frame)
+    cv2.putText(frame, "Kisi Sayisi: " + str(len(kisi_sayisi)), (20,60), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3)
+    cv2.imshow('Mikrodenetleyiciler',frame)
     if cv2.waitKey(1)&0xFF==27:
         break
 
